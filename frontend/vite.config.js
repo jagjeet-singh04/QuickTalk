49a37a427c1d0ea0b023a9abe96990a4f9b9c25c
Jagjeet <jagjeetsingh0424@gmail.com>
2025-06-19 07:20:35 +0530

    error fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , tailwindcss()]
-  
-})
+  plugins: [react() , tailwindcss()] , 
+  build: {
+    outDir: 'public',  // This matches the distDir in vercel.json
+    
+    }
+
+  }
+)
